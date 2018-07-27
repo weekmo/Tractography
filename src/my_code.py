7ed53d15def8712db79989812971ca95a487cb84
weekmo <weekmo@aol.com>
2018-07-27 11:44:15 +0200

    start working on data

--- src/my_code.py
+++ src/my_code.py
@@ -3,8 +3,13 @@ Created on 24 Jul 2018
 
 @author: mohammed
 '''
-from plyfile import PlyData,PlyElement
-#from os import listdir
-#print(listdir("../data"))
-ply_data = PlyData.read('../data/atr-left_shore.ply')
-print(ply_data.elements[0].properties)
+from plyfile import PlyData, PlyElement
+import playStruct as ps
+
+path = '../data/134223/atr-left_tckgen.ply'
+# ply_data = PlyData.read(path)
+my_ply = ps.plyStruct()
+my_ply.loadPolyData(path)
+#print(my_ply.idx)
+print(my_ply.feDict['x_idx'][:10])
+print(my_ply.feDict['x'][:10])
