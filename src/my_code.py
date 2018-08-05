c1118b3aa78b9a17e63c429d6a16abc3a910c1d5
weekmo <weekmo@aol.com>
2018-08-05 17:25:30 +0200

    organize the code

--- src/my_code.py
+++ src/my_code.py
@@ -3,35 +3,21 @@ Created on 24 Jul 2018
 
 @author: mohammed
 '''
-
-from shutil import copyfile
-from dipy.align.streamlinear import StreamlineLinearRegistration
-#from streamlinear import StreamlineLinearRegistration
-from dipy.tracking.streamline import set_number_of_points
+from tractographyPly import read,write,show_bundles,register,register_all
 from dipy.viz import window
-from tractographyPly import TractographyPly,show_both_bundles,write_to_file
-def register(traget_path,subject_path,points=40):
-    tract1 = TractographyPly(traget_path)
-    tract2 = TractographyPly(subject_path)
+#register_all('../data/')
+aligned_bundle=register(traget_path= '../data/150019/m_ex_atr-left_shore.ply',
+                        subject_path='../data/164939/m_ex_atr-left_shore.ply')
 
-    cb_subj1 = set_number_of_points(tract1.data, points)
-    cb_subj2 = set_number_of_points(tract2.data, points)
-
-    srr = StreamlineLinearRegistration()
-    srm = srr.optimize(static=cb_subj1, moving=cb_subj2)
-    #print(srm.matrix)
-    return srm.transform(tract2.data)
-"""
-copyfile(path1,data_path+'subject_target.ply')
-copyfile(path2,data_path+'subject_before.ply')
-write_to_file('../data/subject_after.ply',cb_subj2_aligned)
-
-show_both_bundles([tract1.data, tract2.data],
-                  colors=[window.colors.orange, window.colors.red],
+data1 = read('../data/150019/m_ex_atr-left_shore.ply')
+data2 = read('../data/164939/m_ex_atr-left_shore.ply')
+write('../data/test.ply',aligned_bundle)
+show_bundles([data1, data2,aligned_bundle],
+                  colors=[window.colors.orange, window.colors.red,window.colors.blue],
                   show=False,
                   fname='../data/before_registration.png')
-
-show_both_bundles([tract2.data, cb_subj2_aligned],
+"""
+show_bundles([data1, aligned_bundle],
                   colors=[window.colors.orange, window.colors.red],
                   show=False,
                   fname='../data/after_registration.png')
--- src/tractographyPly.py
+++ src/tractographyPly.py
@@ -1,45 +1,28 @@
-from plyfile import PlyData, PlyElement
-import numpy as np
-from dipy.viz import window, actor
-from time import sleep
-class TractographyPly:
-    def __init__(self,fname):
-        self.ply = PlyData.read(fname)
-        self.vertices_count = len(self.ply.elements[0].data)
-        self.fiber_count=len(self.ply.elements[0].data)
-        self.data = []
-        self.indeces = np.asarray([i[0] for i in self.ply.elements[1].data])
-        temp=[]
-        for i,item in enumerate(self.ply.elements[0].data):
-            i+=1
-            temp.append([item[3],item[4],item[5]])
-            if i in self.indeces:
-                self.data.append(np.asarray(temp))
-                temp=[]
-        #self.data=np.asarray(self.data)
+def read(fname):
+    from plyfile import PlyData, PlyElement
+    import numpy as np
 
-    def writ_to_file(self,fname):
-        with open(fname,'w') as f:
-            f.write('ply\n')
-            f.write('format ascii 1.0\n')
-            for i in self.ply.comments:
-                f.write('comment {}\n'.format(i))
-            for elem in self.ply.elements:
-                f.write('element {} {}\n'.format(elem.name,len(elem.data)))
-                if(len(elem.properties)>1):
-                    for i in range(3):
-                        f.write(str(elem.properties[i])+'\n')
-                else:
-                    f.write(str(elem.properties[0])+'\n')
-            f.write('end_header\n')
-            for elem in self.ply.elements[0].data:
-                f.write("{} {} {}\n".format(elem[3],elem[4],elem[5]))
-            for elem in self.ply.elements[1].data:
-                f.write(str(elem[0])+'\n')
+    ply = PlyData.read(fname)
+    data = []
+    indeces = np.asarray([i[0] for i in ply.elements[1].data])
+    temp=[]
+    for i,item in enumerate(ply.elements[0].data):
+        i+=1
+        temp.append([item[3],item[4],item[5]])
+        if i in indeces:
+            data.append(np.asarray(temp))
+            temp=[]
+    del temp
+    del indeces
+    del ply
+    return data
 
-def show_both_bundles(bundles, colors=None, show=True, fname=None):
+def show_bundles(bundles, colors=None, show=True, fname=None):
+    from dipy.viz import window, actor
+    from time import sleep
+
     ren = window.Renderer()
-    ren.SetBackground(1., 1, 1)
+    ren.SetBackground(1, 1, 1)
     for (i, bundle) in enumerate(bundles):
         color = colors[i]
         lines_actor = actor.streamtube(bundle, color, linewidth=0.3)
@@ -52,21 +35,68 @@ def show_both_bundles(bundles, colors=None, show=True, fname=None):
         sleep(1)
         window.record(ren, n_frames=1, out_path=fname, size=(900, 900))
 
-def write_to_file(fname,data,comments=None):
+def write(fname,data,
+          comments=['DTI Tractography, produced by fiber-track']):
     data_txt=''
     vert_num=0
     indeces=[]
     index=0
     with open(fname,'w') as f:
         for fib in data:
-            indeces.append(len(fib)+index)
+            index+=len(fib)
+            indeces.append(index)
             for vert in fib:
                 data_txt+=str(vert[0])+' '+str(vert[1])+' '+str(vert[2])+'\n'
                 vert_num+=1
-            index+=len(fib)
-        f.write('ply\nformat ascii 1.0\ncomment DTI Tractography, produced by fiber-track\nelement vertices '+str(vert_num))
+        f.write('ply\nformat ascii 1.0\n')
+        for com in comments:
+            f.write('comment '+com+'\n')
+        f.write('element vertices '+str(vert_num))
         f.write('\nproperty float x\nproperty float y\nproperty float z\n')
         f.write('element fiber {}\nproperty int endindex'.format(len(indeces)))
         f.write('\nend_header\n'+data_txt)
         for i in indeces:
             f.write(str(i)+'\n')
+
+    del data_txt
+    del indeces
+
+def register(traget_path,subject_path,points=40):
+    from dipy.align.streamlinear import StreamlineLinearRegistration
+    from dipy.tracking.streamline import set_number_of_points
+
+    bundle1 = read(traget_path)
+    bundle2 = read(subject_path)
+
+    cb_subj1 = set_number_of_points(bundle1, points)
+    cb_subj2 = set_number_of_points(bundle2, points)
+
+    srr = StreamlineLinearRegistration()
+    srm = srr.optimize(static=cb_subj1, moving=cb_subj2)
+
+    return srm.transform(bundle2)
+
+def register_all(data_path):
+    from os import listdir,mkdir
+    from os.path import isfile,isdir
+
+    files_names={}
+    index=0
+    target_key=''
+    for dir in listdir(data_path):
+        dir=data_path+dir+'/'
+        if isdir(dir):
+            files_names[dir]=[f for f in listdir(dir) if isfile(dir+f)]
+
+    for k in files_names.keys():
+        if index ==0:
+            target_key=k
+        else:
+            output_path=k[:-1]+'_output/'
+            if not isdir(output_path):
+                mkdir(output_path)
+            for target_path,subject_path in zip(files_names[target_key],files_names[k]):
+                if target_path == subject_path:
+                    aligned_subject=register(target_key+target_path,k+subject_path)
+                    write(output_path+subject_path,aligned_subject)
+        index+=1
