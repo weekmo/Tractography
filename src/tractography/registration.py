a2fd7ce39367a997d6e20c89e2ebe0147c2cf924
weekmo <weekmo@aol.com>
2018-09-05 12:03:24 +0200

    organize register_all

--- src/tractography/registration.py
+++ src/tractography/registration.py
@@ -32,63 +32,9 @@ def register(target, subject,points=20):
     del cb_subj1
     del cb_subj2
     del target
-    print(srm.matrix)
-    return srm.transform(subject)
+    return srm.transform(subject),srm.matrix
 
 def register_all(data_path):
-
-    r""" Register all ply files in a folder
-
-    :param data_path: str,
-        - A folder has subjects each in a folder as ply file format.
-        - It will not read ply images putted directly in this folder but inside folders.
-        - The subject in the first folder will be targets and the others are moved subjects
-    :return: files,
-        It wil export aligned subject to trk files each in a new folder as the same name as subject plus _out
-    """
-    import time
-    from os import listdir, mkdir
-    from os.path import isfile, isdir
-    from tractography.io import read_ply,write_trk,write_ply
-    files_names = {}
-    index = 0
-    target_key = ''
-    time_list={}
-    for dir in listdir(data_path):
-        dir = data_path+'/in/' + dir+'/'
-        if isdir(dir):
-            files_names[dir] = [f for f in listdir(dir) if isfile(dir + f)]
-    for k in files_names.keys():
-        if index == 0:
-            target_key = k
-            index+=1
-        else:
-            output_path = 'out/'+k
-            if not isdir(output_path):
-                mkdir(output_path)
-            for target_path, subject_path in zip(files_names[target_key], files_names[k]):
-                if target_path == subject_path:
-                    out_file_name = output_path + subject_path
-                    time_list[out_file_name]={}
-
-                    target = read_ply(target_key + target_path)
-
-                    start_time = time.clock()
-                    subject = read_ply(k + subject_path)
-                    time_list[out_file_name]['Load Time:'] = time.clock()-start_time
-
-                    start_time = time.clock()
-                    aligned_subject = register(target, subject)
-                    time_list[out_file_name]['Registration Time:'] = time.clock()-start_time
-
-                    start_time = time.clock()
-                    write_ply(out_file_name, aligned_subject)
-                    write_trk(out_file_name+'.trk', aligned_subject)
-                    time_list[out_file_name]['Writing Time:'] = time.clock()-start_time
-    del files_names
-    return time_list
-
-def register_all2(data_path):
 
     r""" Register all ply files in a folder
 
@@ -119,14 +65,14 @@ def register_all2(data_path):
             if isfile(subject_path):
                 start_time = time.clock()
                 subject=read_ply(subject_path)
-                time_list[f]['\tLoading Subject '+dirs[i]]=time.clock()-start_time
+                time_list[f]['Loading Subject '+dirs[i]]=time.clock()-start_time
                 start_time = time.clock()
                 aligned_subject = register(target=target,subject=subject)
-                time_list[f]['\tAlign Subject '+dirs[i]]=time.clock()-start_time
+                time_list[f]['Align Subject '+dirs[i]]=time.clock()-start_time
                 start_time = time.clock()
                 write_ply(out_path, aligned_subject)
                 write_trk(out_path+'.trk', aligned_subject)
-                time_list[f]['\tWriting '+dirs[i]]=time.clock()-start_time
+                time_list[f]['Writing '+dirs[i]]=time.clock()-start_time
     del dirs
     del target_dir
     del files
--- test/implementation.py
+++ test/implementation.py
@@ -7,8 +7,8 @@ Created on 24 Jul 2018
 #from shutil import copyfile
 import numpy as np
 from dipy.tracking.streamline import transform_streamlines
-from tractography.io import read_ply,write_trk,write_ply
-from tractography.registration import register,register_all
+from src.tractography.io import read_ply,write_trk,write_ply
+from src.tractography.registration import register,register_all
 
 data1 = read_ply('data/132118/m_ex_atr-left_shore.ply')
 data2 = read_ply('data/132118/m_ex_atr-right_shore.ply')
@@ -16,28 +16,34 @@ data2 = read_ply('data/132118/m_ex_atr-right_shore.ply')
 '''Fake registration'''
 mat = np.random.rand(4,4)*10
 mat[3,0],mat[3,1],mat[3,2],mat[3,3]=0,0,0,1
+print(mat)
+print(np.linalg.inv(mat))
 new_sl = transform_streamlines(data1,mat)
+write_ply('data/random_transformation.ply',new_sl)
+write_trk('data/random_transformation.trk',new_sl)
 new_sl=register(data1,new_sl)
-write_ply('data/fake_align.ply',new_sl)
-write_trk('data/fake_align.trk',new_sl)
+write_ply('data/realign_random.ply',new_sl)
+write_trk('data/realign_random.trk',new_sl)
+write_ply('data/terget.ply',data1)
 
-''' Right to left'''
+'''
+
 new_sl=register(data1,data2)
-write_ply('data/fake_align.ply',new_sl)
-write_trk('data/fake_align.trk',new_sl)
+write_ply('data/LTR_align.ply',new_sl)
+write_trk('data/LTR_align.trk',new_sl)
 
 write_ply('data/terget.ply',data1)
 write_trk('data/target.trk',data1)
-
-
-register_all('data/')
+'''
+'''
+#register_all('data/')
 # write_trk("../data/my_streamlines0.trk", data1)
 r""" Test fake transformation matrix
 mat = np.random.rand(4,4)*10
 mat[3,0],mat[3,1],mat[3,2],mat[3,3]=0,0,0,1
 print("Trans Mat:\n",mat)
 test_same_bundle=transform_streamlines(data1,mat)
-"""
+
 r""" Show
 show_bundles([data1, test_same_bundle],
                   colors=[window.colors.orange, window.colors.blue],
@@ -48,4 +54,4 @@ show_bundles([data1, after],
                   colors=[window.colors.orange, window.colors.blue],
                   show=False,
                   fname='../data/after_registration.png')
-"""
+'''
