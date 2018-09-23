29b9516726e68c4b418fc2b45edcf45923fe5e3d
weekmo <weekmo@aol.com>
2018-09-23 14:43:21 +0200

    try hard pca

--- test/test4.py
+++ test/test4.py
@@ -1,33 +1,51 @@
 import numpy as np
-#from numpy.linalg import eig
-from sklearn.decomposition import PCA #,IncrementalPCA
+# from numpy.linalg import eig
+from sklearn.decomposition import PCA  # ,IncrementalPCA
 from src.tractography.viz import draw_brain
 from src.tractography.io import read_ply
 from dipy.align.streamlinear import compose_matrix44
 from dipy.tracking.streamline import (transform_streamlines)
 
-#mat = compose_matrix44([0,0,0,0,90,90])
+mat = compose_matrix44([20, 20, 20, 90, 90, 90])
 target = read_ply('../data/132118/m_ex_atr-left_shore.ply')
-#new_tar = transform_streamlines(target,mat)
-#center = np.mean(np.concatenate(target, axis=0), axis=0)
-
+new_tar = transform_streamlines(target, mat)
+# center = np.mean(np.concatenate(target, axis=0), axis=0)
+target = target[0]
+subject = new_tar[0]
 """
 np.random.seed(5)
-data = np.array(np.random.randint(10,size=(10,3)))
+data = np.array(np.random.randint(5,size=(5,3)))
 """
 
 pca = PCA(n_components=3)
 # IncrementalPCA(batch_size=10)
-pca.fit(target[0])
-#target_T = pca.transform(target[0])
-#target_T = pca.fit_transform(target[0])
 
-#pca.fit(new_tar[0])
-#new_tar_T = pca.transform(new_tar[0])
-#new_tar_T = pca.fit_transform(new_tar[0])
+# target_T = pca.transform(target[0])
+# target_T = pca.fit_transform(target[0])
 
-print(pca.components_ )
-#draw_brain([[target[0]],[new_tar[0]],[target_T],[new_tar_T]],[[1,0,0],[0,0,1],[.8,0,0],[0,0,.8]])
+# pca.fit(new_tar[0])
+# new_tar_T = pca.transform(new_tar[0])
+# new_tar_T = pca.fit_transform(new_tar[0])
+# x = pca.fit_transform(target)
+# print("-------- Target ---------")
+# print(data)
+# print("\n-------- X transform ---------")
+# print(x)
+pca = pca.fit(target)
+com = pca.components_
+# mean = np.mean(target, axis=0)
+# y = target - mean
+# y = np.dot(y, pca.components_.T)
+print("----------")
+pca = pca.fit(subject)
+com = np.dot(pca.components_.T, com)
+# mean = np.mean(subject,axis=0)/2
+# x = y + mean
+x = np.dot(target, com)
+
+# print("\n-------- X dot ---------")
+# print(y)
+draw_brain([[target], [subject], [x]], [[1, 0, 0], [0, 0, 1], [0, 1, 0]])
 
 """
 x = np.random.random((20,3))
